2ebf95ec954ac80eeb8d00cec15c96a4d29bd0c4
Khushi Nikam <92162945+KhushiN893@users.noreply.github.com>
2022-12-06 23:38:50 +0530

    Program to Calculate GCD

---
+++ RECURSION/GCD.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int gcdCalculate(int, int);
+int main()
+{
+
+    int a, b;
+    printf("Enter a :");
+    scanf("%d", &a);
+    printf("\nEnter b :");
+    scanf("%d", &b);
+    printf("%d & %d is ", gcdCalculate(a, b));
+    return 0;
+}
+int gcdCalculate(int a, int b)
+{
+    if (b == 0)
+        return a;
+    if (a == 0)
+        return b;
+    return gcdCalculate(b, a % b);
+}
