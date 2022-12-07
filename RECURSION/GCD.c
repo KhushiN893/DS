bb4503b7e4a2da46fc3e1aca0a2047b5c966d8b7
Khushi Nikam <92162945+KhushiN893@users.noreply.github.com>
2022-12-07 22:08:59 +0530

    Update GCD.c

--- RECURSION/GCD.c
+++ RECURSION/GCD.c
@@ -1,14 +1,14 @@
+// Find Greatest Common Divisor
 #include <stdio.h>
 int gcdCalculate(int, int);
 int main()
 {
-
     int a, b;
     printf("Enter a :");
     scanf("%d", &a);
     printf("\nEnter b :");
     scanf("%d", &b);
-    printf("%d & %d is ", gcdCalculate(a, b));
+    printf("%d & %d is %d", a, b, gcdCalculate(a, b));
     return 0;
 }
 int gcdCalculate(int a, int b)
